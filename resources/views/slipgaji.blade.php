f55824649d399bedb2b564fa687e1a752ffddc47
jstalinko <alinkodotid@gmail.com>
2024-11-01 22:13:13 +0700

    add date pdf

--- resources/views/slipgaji.blade.php
+++ resources/views/slipgaji.blade.php
@@ -252,6 +252,11 @@
 
     <div class="gaji-bersih">
       <h3>Gaji Bersih: Rp. {{ number_format(($data->in_gaji_pokok + $data->in_upah_lembur + $data->in_uang_makan + $data->in_uang_transport + $data->in_lain) - ($data->out_telat + $data->out_kerusakan_barang + $data->out_kasbon + $data->out_lain), 0, ',', '.') }}</h3> 
+       
+      <b>
+        GAJI SUDAH DI TRANSFER KE *({{$data->karyawan->bank_name}}) {{$data->karyawan->account_number}} a/n {{$data->karyawan->account_name}}*
+
+        </b>
     </div>
     @endif
   </div>
